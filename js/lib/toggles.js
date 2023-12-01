fa14bbd765718f226a4e1728affe479adbe05d56
JacobLinkous <jacobmlinkous@gmail.com>
2023-12-01 16:08:06 -0600

    toggles and format

--- js/lib/toggles.js
+++ js/lib/toggles.js
@@ -7,3 +7,42 @@ $('#btnCancel').on('click', function () {
     $('#loginCard').slideDown();
     $('#registerCard').slideUp();
 });
+
+function closeAll(){
+    $('#homepage').slideUp();
+    $('#lights').slideUp();
+    $('#temperature').slideUp();
+    $('#door').slideUp();
+    $('#foodandwater').slideUp();
+    $('#egg').slideUp();
+};
+
+$('#homepagebtn').on('click', function () {
+    closeAll()
+    $('#homepage').slideDown();
+});
+
+$('#lightsbtn').on('click', function () {
+    closeAll();
+    $('#lights').slideDown();
+});
+
+$('#tempbtn').on('click', function () {
+    closeAll();
+    $('#temperature').slideDown();
+});
+
+$('#doorbtn').on('click', function () {
+    closeAll();
+    $('#door').slideDown();
+});
+
+$('#foodandwaterbtn').on('click', function () {
+    closeAll();
+    $('#foodandwater').slideDown();
+});
+
+$('#eggbtn').on('click', function () {
+    closeAll();
+    $('#egg').slideDown();
+});
