fa5936fabf43d3e077ded99feae4c48db57d9f1e
JacobLinkous <jacobmlinkous@gmail.com>
2023-12-03 22:00:18 -0600

    code cleanup

--- js/lib/dashboard.js
+++ js/lib/dashboard.js
@@ -1,9 +1,3 @@
-/*
-for profile to get address
-$.get('https://simplecoop.swollenhippo.com/coop.php?SessionID=5e88dbee-4e00-430d-b3f2-44f10d8c4777',function(result){
-        console.log(result.ZIP);
-    });
-*/
 let FoodAlarmStart = true;
 let WaterAlarmStart = true;
 
